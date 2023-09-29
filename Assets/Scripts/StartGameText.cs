90caceac2dec812007ca9395a071259bb9686c97
Gido <130654343+Egg321123@users.noreply.github.com>
2023-09-29 10:41:29 +0200

    feat: added screenshake
    screenshake added for better game feel
    added scripts that has the camera shake anytime the ball collides with a ball or player

--- Assets/Scripts/StartGameText.cs
+++ Assets/Scripts/StartGameText.cs
@@ -4,10 +4,24 @@ public class StartGameText : MonoBehaviour
 {
     BallMovement ballMovement;
     GameObject playText;
+    GameObject empty;
+    Camera camera;
+    
+    private void Awake()
+    {
+        Camera.main.gameObject.AddComponent<Screenshake>();
+    }
     void Start()
     {
         ballMovement = GameObject.FindGameObjectWithTag("Player").GetComponent<BallMovement>();
+        ballMovement.gameObject.AddComponent<BallBounceCheck>();
         playText = GameObject.FindGameObjectWithTag("PlayText");
+        empty = new GameObject()
+        {
+            name = "cameraHolder"
+        };
+        camera = FindObjectOfType<Camera>();
+        camera.transform.parent = empty.transform;
     }
 
     void Update()
