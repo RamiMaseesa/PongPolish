2c1fd140d5684d66feb47cdb65fbb2623593f702
Rami Maseesa <132995650+RamiMaseesa@users.noreply.github.com>
2023-09-29 13:56:50 +0200

    feat: trail to player

--- Assets/Scripts/Rami/BallParticleEffect.cs
+++ Assets/Scripts/Rami/BallParticleEffect.cs
@@ -6,19 +6,20 @@ using UnityEngine;
 public class BallParticleEffect : GetComponents
 {
     private TrailRenderer trail;
-
+    private TrailRenderer playerTrail;
     protected override void Start()
     {
         base.Start();
 
-        trail = ball.gameObject.AddComponent<TrailRenderer>();
-
         TrailOneSettings();
-
+        GivePlanesTrail();
     }
-
     private void TrailOneSettings()
     {
+        //add a TrailRenderer component to the ball
+        trail = ball.gameObject.AddComponent<TrailRenderer>();
+
+        // change the settings
         trail.startColor = new Color(0.0f, 0.0f, 1.0f, 1.0f);
         trail.endColor = new Color(1.0f, 0.0f, 1.0f, 0.0f);
 
@@ -29,6 +30,26 @@ public class BallParticleEffect : GetComponents
         trail.material.renderQueue = 1;
 
         trail.time = .5f;
+    }
+
+    private void GivePlanesTrail()
+    {
+        // for each object in players add trailrenderer componenet
+        foreach(PlayerMovement player in players)
+        {
+            playerTrail = player.gameObject.AddComponent<TrailRenderer>();
+
+            playerTrail.startColor = new Color(0.0f, 0.0f, 1.0f, 1.0f);
+            playerTrail.endColor = new Color(1.0f, 0.0f, 1.0f, 0.0f);
+
+            playerTrail.startWidth = 1f;
+            playerTrail.endWidth = 0.1f;
+
+            playerTrail.material = new Material(Shader.Find("Sprites/Default"));
+            playerTrail.material.renderQueue = 0;
+
+            playerTrail.time = 1f;
+        }
     }
 
 }
